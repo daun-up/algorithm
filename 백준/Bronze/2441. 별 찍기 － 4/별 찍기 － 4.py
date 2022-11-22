2ade1d21b35eddc9710c3e44f64408f9b5be5332
jo_daun <105338882+daun-up@users.noreply.github.com>
2022-11-22 14:22:13 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.py
@@ -0,0 +1,13 @@
+a = int(input())
+for i in range(1,a+1):
+    for j in range(i-1):
+        print(" ", end ="")
+    for j in range(a+1-i):
+        print("*", end = '')
+    print()
+
+
+
+
+    
+
