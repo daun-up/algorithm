d5f67312d5356433bc26cf0dbb75013d3a07973e
jo_daun <105338882+daun-up@users.noreply.github.com>
2023-04-05 11:46:13 +0900

    [Silver V] Title: 덩치, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input()) #전체 사람의 수
+info = []
+rank = []
+
+for _ in range(n):
+    x,y = map(int,input().split())
+    info.append((x,y))
+
+for i in info:
+    rank = 1
+    for j in info:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+
+    
+    print(rank,end=' ')
+
+
+
+
+
+
+
+    
+
+
+ 
