f11ef9dbc51fef3c031dcaa337db691929e4a443
jo_daun <105338882+daun-up@users.noreply.github.com>
2023-04-06 12:39:52 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long int a, b, c;
+    long long int result;
+    cin >> a >> b >> c ;
+    result = a + b + c;
+    cout << result << endl;
+}
