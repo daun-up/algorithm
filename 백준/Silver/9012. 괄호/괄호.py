1c49dc594f6766f487595f47e96eb3b85407c962
jo_daun <105338882+daun-up@users.noreply.github.com>
2023-05-11 01:36:59 +0900

    [Silver IV] Title: 괄호, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    s = input()
+
+    stack = []
+    for c in s:
+        if c == "(":
+            stack.append(c)
+        elif c == ")":
+            if not stack or stack[-1] != "(":
+                stack.append(")")
+                break
+            stack.pop()
+    print("YES" if not stack else "NO")
