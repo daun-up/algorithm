20f821c0dd5b0258a19af32661dccb9b473788fd
jo_daun <105338882+daun-up@users.noreply.github.com>
2023-08-13 23:26:12 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,16 @@
+n, k = map(int,input().split())
+
+coins = [int(input()) for _ in range(n)]
+
+coins.sort(reverse=True)
+
+result = 0
+
+for coin in coins :
+    if k // coin > 0 :
+        result += k // coin
+    k %= coin
+
+print(result)
+
+
