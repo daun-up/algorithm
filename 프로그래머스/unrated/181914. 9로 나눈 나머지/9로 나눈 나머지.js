4812ac93a249f303ee26353faa1d43a7ca1f6622
Jodaun <105338882+daun-up@users.noreply.github.com>
2023-09-20 21:56:27 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.js
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.js
@@ -1,9 +1,4 @@
 function solution(number) {
-    var answer = 0;
-    let array = number.split("").map(Number);
-    for (let i = 0; i < array.length; i++) {
-        answer += array[i];
-    }
-    answer = answer % 9;
-    return answer;
+    var answer = number.split("").map(Number);
+    return answer.reduce((a, b) => a + b) % 9;
 }
