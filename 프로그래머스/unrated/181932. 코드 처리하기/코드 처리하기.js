00d7765ed9eb09aeecfd7f2369511f19f13bf7e1
Jodaun <105338882+daun-up@users.noreply.github.com>
2023-09-20 21:54:37 +0900

    [unrated] Title: 코드 처리하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -1,30 +1,21 @@
 function solution(code) {
     var answer = '';
     let mode = 0;
-    array = code.split("");
-    for (let i = 0; i < array.length ; i++) {
+    for (let i = 0; i < code.length ; i++) {
         if (mode === 1) {
-            if(array[i] == 1) {
+            if(code[i] == 1) {
                 mode = 0;
             } else { // 1 이 아닐 때
-                if(i % 2 !== 0) {
-                    answer += array[i];
-                }
+                answer = i%2 === 0 ? answer : answer + code[i];
             }
-            
-            
         } else { // mode === 0
-            if(array[i] == 1){
+            if(code[i] == 1){
                 mode = 1;
             } else {
-                if (i % 2 === 0) {
-                    answer += array[i];
-                }
+                answer = i%2 === 0 ? answer + code[i] : answer;
             }
         } 
     }
-    if (answer.length == 0) {
-        return "EMPTY";
-    } else {return answer;}
-   
+    
+    return answer.length === 0 ? "EMPTY" : answer;
 }
