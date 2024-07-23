4f4c3aab4713612fa8b4c60ce5cc5220b89fb949
Jodaun <105338882+daun-up@users.noreply.github.com>
2024-07-23 15:48:02 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,10 +1,7 @@
 n = int(input())
-d = [0] * 1001
-
-d[1] = 1
-d[2] = 3
+d = [0,1,3]
 
 for i in range(3, n+1):
-    d[i] = d[i-1] + 2 * d[i-2]
+    d.append(d[i-1] + 2 * d[i-2])
 
 print(d[n] % 10007)
