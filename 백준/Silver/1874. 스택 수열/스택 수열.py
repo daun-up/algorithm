6760365c3403365e4144409e3b18f448b66e458b
Jodaun <105338882+daun-up@users.noreply.github.com>
2025-01-01 21:42:11 +0900

    [Silver II] Title: 스택 수열, Time: 2676 ms, Memory: 35020 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,26 +1,37 @@
-import sys
-from collections import deque
+# 스택에 "push" 하는 순서가 오름차순이다.
+# 자료를 넣는 (push) 입구와 자료를 뽑는 (pop) 입구가 같아 나중에 들어간 자료가 가장 먼저 나오는 특징을 가지고 있다.
 
-stack = []
-result = []
+n = int(input())
 
-n = int(sys.stdin.readline().strip())
-num = 1
+stack = [] # 여기에 추가
+answer = [] # 수열을 저장
+cnt = 1
+flag = True
 
-for i in range(n) :
-    value = int(sys.stdin.readline().strip())
-    while (num <= value) :
-        stack.append(num)
-        result.append("+")
-        num += 1
-        
-    if stack[-1] == value :
+for _ in range(n) :
+    num = int(input())
+    
+    # num 이하 숫자까지 stack 에 넣기
+    while cnt <= num :
+        stack.append(cnt)
+        answer.append('+')
+        cnt += 1
+    
+    # num 이랑 스택 맨 위 숫자가 동일하다면 제거
+    if stack[-1] == num :
         stack.pop()
-        result.append("-")
-    else:
-        result.clear()
-        result.append('NO')
-        break	
+        answer.append('-')
+    else :
+        # print('NO')
+        # break 반복문만 종료됨
+        # sys.exit() 라이브러리를 사용해야 함
+        
+        flag = False
+        break
+        
 
-for k in result :
-    print(k)
+if flag :
+    for i in answer :
+        print(i)
+else :
+    print("NO")
