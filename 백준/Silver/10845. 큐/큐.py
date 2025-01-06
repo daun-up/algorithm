4be9663892ad2fba88928bf6f210b81e04603322
Jodaun <105338882+daun-up@users.noreply.github.com>
2025-01-06 15:13:09 +0900

    [Silver IV] Title: 큐, Time: 68 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,31 +1,36 @@
+from collections import deque
 import sys
 
-N = int(sys.stdin.readline().strip())
+input = sys.stdin.readline
 
-stack = []
-for i in range(N):
-  cmd = sys.stdin.readline().strip().split()
-  if cmd[0] == 'push':
-    stack.append(cmd[1])
-  elif cmd[0] == 'pop':
-    if len(stack) != 0:
-      print(stack.pop(0))
-    else:
-      print(-1)
-  elif cmd[0] == 'size':
-    print(len(stack))
-  elif cmd[0] == 'empty':
-    if len(stack) == 0:
-      print(1)
-    else:
-      print(0)
-  elif cmd[0] == 'front':
-    if len(stack) != 0:
-      print(stack[0])
-    else:
-      print(-1)
-  elif cmd[0] == 'back':
-    if len(stack) != 0:
-      print(stack[-1])
-    else:
-      print(-1)
+n = int(input().rstrip())
+
+q = deque()
+
+for _ in range(n) :
+    command = input().rstrip().split()
+    
+    if command[0] == "pop" :
+        if len(q) == 0 :
+            print(-1)
+        else :
+            print(q.popleft())
+    elif command[0] == "size" :
+        print(len(q))
+    elif command[0] == "empty" :
+        if len(q) == 0 :
+            print(1)
+        else :
+            print(0)
+    elif command[0] == "front" :
+        if len(q) == 0 :
+            print(-1)
+        else:
+            print(q[0])
+    elif command[0] == "back" :
+        if len(q) == 0 :
+            print(-1)
+        else :
+            print(q[-1])
+    elif command[0] == "push" :
+        q.append(int(command[1]))
