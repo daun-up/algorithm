23e108f3d508a7a5ba2d43fbcdeeab568a3a1918
Jodaun <105338882+daun-up@users.noreply.github.com>
2025-01-08 14:48:32 +0900

    [Silver IV] Title: 덱, Time: 64 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,35 +1,41 @@
 from collections import deque
 import sys
- 
-N = int(sys.stdin.readline().strip())
-deque = deque()
+input = sys.stdin.readline
 
-for i in range(N) :
-    cmd = sys.stdin.readline().strip().split()
-    if (cmd[0] == 'push_back') :
-        deque.append(cmd[1])
-    elif (cmd[0] == 'push_front') :
-        deque.appendleft(cmd[1])
-    elif (cmd[0] == 'pop_front') :
-        if len(deque) == 0:
+n = int(input().rstrip())
+q = deque()
+
+for _ in range(n) :
+    cmd = input().rstrip().split()
+    
+    if cmd[0] == 'push_front' :
+        q.appendleft(cmd[1])
+    elif cmd[0] == 'push_back' :
+        q.append(cmd[1])
+    elif cmd[0] == 'pop_front' :
+        if q:
+            print(q.popleft())
+        else :
             print(-1)
-        else : print(deque.popleft())
-    elif (cmd[0] == 'pop_back') :
-        if len(deque) == 0:
+    elif cmd[0] == 'pop_back' :
+        if q:
+            print(q.pop())
+        else :
             print(-1)
-        else : print(deque.pop())
-    elif (cmd[0] == 'size') :
-        print(len(deque))
-    elif (cmd[0] == 'empty') :
-        if len(deque) == 0:
+    elif cmd[0] == 'size':
+            print(len(q))
+    elif cmd[0] == 'empty':
+        if q :
+            print(0)
+        else :
             print(1)
-        else : print(0)
-    elif (cmd[0] == 'front'):
-        if len(deque) != 0:
-            print(deque[0])
-        else : print(-1)
-    elif (cmd[0] == 'back'):
-        if len(deque) != 0:
-            print(deque[-1])
-        else : print(-1)
-        
+    elif cmd[0] == 'front' :
+        if q :
+            print(q[0])
+        else :
+            print(-1)
+    elif cmd[0] == 'back' :
+        if q :
+            print(q[-1])
+        else :
+            print(-1)
