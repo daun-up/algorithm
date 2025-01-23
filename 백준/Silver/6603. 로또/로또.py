a99505ac7995a20a8616f8f7b8ec58949de2b4d0
Jodaun <105338882+daun-up@users.noreply.github.com>
2025-01-23 13:48:57 +0900

    [Silver II] Title: 로또, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,19 +1,17 @@
-def backtracking(lotto, s, start):
-    if len(lotto) == 6:
-        print(*lotto)
+# 재귀를 사용하여 번호 조합을 만들어냄
+def lotto(arr, s, idx, cnt) : # 선택한 번호를 저장할 배열, 입력 받은 숫자 리스트, 다음 반복문의 시작 위치, 지금까지 선택한 번호의 수
+    if cnt == 6 :
+        print(*arr)
         return
-
-    for i in range(start, len(s)):
-        backtracking(lotto + [s[i]], s, i + 1)
-
-
-while True:
-    data = list(map(int, input().split()))
-    k = data[0]  
-    s = data[1:]  
-
-    if k == 0:
+    for i in range(idx, len(s)) :
+        arr[cnt] = s[i]
+        # 선택한 번호를 바탕으로 다음 조합 만들기 (백트래킹)
+        lotto(arr, s, i + 1, cnt + 1)
+        
+while True :
+    s = list(map(int,input().split()))
+    if s[0] == 0 : # 리스트 첫 번째 숫자가 0 이면 종료
         break
-
-    backtracking([], s, 0)  # 빈 리스트에서 시작하여 백트래킹
+    arr = [0] * 6 # 6 개의 번호를 저장할 배열
+    lotto(arr, s[1:], 0, 0) # 리스트의 첫 번째 값(숫자 개수)은 제외하고 나머지 숫자들만 넘김
     print()
