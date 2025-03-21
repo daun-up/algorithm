3389bdec4827d9f5a45f417440daa253ac33c049
Jodaun <105338882+daun-up@users.noreply.github.com>
2025-03-21 13:10:13 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,21 +1,24 @@
-n = int(input())
-m = int(input())
+def dfs(c) :
+    global ans
+    v[c] = 1 # 단위 작업
+    ans += 1 # 단위 작업
+    
+    for n in adj[c] : # C 와 연결된 노드를 방문 (다음 노드 next)
+        if not v[n] : # 만약에 방문한 노드가 아니먄
+            dfs(n) # dfs 로 해당되는 노드를 방문
 
-graph = [[] for _ in range(n+1)]
+# 여기까지 하고 함수 만들기
+N = int(input())
+M = int(input())
+adj = [[] for _ in range(N + 1)]  # 인접 리스트 초기화
 
-for i in range(m):
-    x, y = map(int, input().split())
+for _ in range(M):
+    s, e = map(int, input().split())
+    adj[s].append(e)
+    adj[e].append(s)  # 양방향 그래프이므로 반대 방향도 추가
+    
+ans = 0
+v = [0] * (N+1)
+dfs(1)
 
-    graph[x].append(y)
-    graph[y].append(x)
-
-visit = [0] * (n+1)
-
-def dfs(graph, v, visited):
-    visit[v] = 1
-    for i in graph[v]:
-        if visit[i] == 0:
-            dfs(graph, i, visited)
-
-dfs(graph, 1, visit)
-print(visit.count(1) - 1)
+print(ans-1)
