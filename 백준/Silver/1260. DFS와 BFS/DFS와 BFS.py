729d3381dd338b2594af9f4d49e8ccd19cbc2767
Jodaun <105338882+daun-up@users.noreply.github.com>
2025-03-21 12:35:27 +0900

    [Silver II] Title: DFS와 BFS, Time: 296 ms, Memory: 33528 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,45 +1,46 @@
-from collections import deque
+def dfs(c):  # 현재 노드 : current
+    ans_dfs.append(c)  # 방문 노드 추가
+    v[c] = 1  # 방문 표시
+    
+    for n in adj[c]:
+        if not v[n]:  # 방문하지 않은 노드이면 DFS 수행
+            dfs(n)
 
+def bfs(s):  # 현재 노드 : start
+    q = []  # 큐 초기화
+    q.append(s)  # Q 에 초기 데이터 삽입
+    ans_bfs.append(s)  # 방문 노드 추가
+    v[s] = 1  # 방문 표시
+    
+    while q:
+        c = q.pop(0)  # 큐에서 노드 꺼내기
+        for n in adj[c]:
+            if not v[n]:  # 방문하지 않은 노드이면 큐에 삽입
+                q.append(n)
+                ans_bfs.append(n)
+                v[n] = 1
 
-def dfs(start):
-    visited[start] = True
-    print(start, end=" ")
-
-    for i in graph[start]:
-        if not visited[i]:
-            dfs(i)
-
-
-def bfs(start):
-    queue = deque([start])
-    visited[start] = True
-    while queue:
-
-        v = queue.popleft()
-        print(v, end=" ")
-        for i in graph[v]:
-            if not visited[i]:
-                visited[i] = True
-                queue.append(i)
-
-
-N, M, V = map(int, input().split())
-graph = [[] for _ in range(N + 1)]
+N, M, V = map(int, input().split())  # 노드 수, 간선 수, 시작 노드
+adj = [[] for _ in range(N + 1)]  # 인접 리스트 초기화
 
 for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    s, e = map(int, input().split())
+    adj[s].append(e)
+    adj[e].append(s)  # 양방향 그래프이므로 반대 방향도 추가
 
-# 정렬
-for i in graph:
-    i.sort()
+# [1] 오름차순 정렬 (문제에서 작은 번호부터 방문해야 하는 경우)
+for i in range(1, N + 1):
+    adj[i].sort()
 
-# dfs
-visited = [False] * (N + 1)
-dfs(V)
-print()
+# DFS 실행
+v = [0] * (N + 1)  # 방문 리스트 초기화
+ans_dfs = []
+dfs(V)  # 시작 노드로 DFS 실행
 
-# bfs
-visited = [False] * (N + 1)
-bfs(V)
+# BFS 실행
+v = [0] * (N + 1)  # 방문 리스트 초기화
+ans_bfs = []
+bfs(V)  # 시작 노드로 BFS 실행
+
+print(*ans_dfs)
+print(*ans_bfs)
