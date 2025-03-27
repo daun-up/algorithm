c1afd3185b9e511476ca397003b97c18e549b261
Jodaun <105338882+daun-up@users.noreply.github.com>
2025-03-27 18:41:08 +0900

    [level 2] Title: 피보나치 수, Time: 19.76 ms, Memory: 12.8 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,7 +1,3 @@
 def solution(n):
-    arr = [0] * (n + 1)
-    arr[0] = 0
-    arr[1] = 1
-    for i in range(2, n + 1):
-        arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567
-    return arr[n]
+    answer = 0
+    return answer
