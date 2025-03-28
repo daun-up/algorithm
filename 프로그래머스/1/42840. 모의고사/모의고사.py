e04be386d0ea0b90aad216dadd0f758f274937c0
Jodaun <105338882+daun-up@users.noreply.github.com>
2025-03-28 19:43:49 +0900

    [level 1] Title: 모의고사, Time: 3.45 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,28 @@
+def solution(answers):
+    answer = []
+    first = [1, 2, 3, 4, 5]
+    second = [2, 1, 2, 3, 2, 4, 2, 5]
+    third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    first_cnt = 0
+    second_cnt = 0
+    third_cnt = 0
+
+    for i in range(len(answers)):
+        if answers[i] == first[i % len(first)]:
+            first_cnt += 1
+        if answers[i] == second[i % len(second)]:
+            second_cnt += 1
+        if answers[i] == third[i % len(third)]:
+            third_cnt += 1
+
+    max_cnt = max(first_cnt, second_cnt, third_cnt)
+
+    if first_cnt == max_cnt:
+        answer.append(1)
+    if second_cnt == max_cnt:
+        answer.append(2)
+    if third_cnt == max_cnt:
+        answer.append(3)
+
+    return answer
