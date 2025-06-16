fe1289e88ebe7d843e046aff7b30b7d693f302bd
Jodaun <105338882+daun-up@users.noreply.github.com>
2025-06-16 12:10:32 +0900

    [Silver II] Title: 부분수열의 합, Time: 208 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -3,27 +3,21 @@ data = list(map(int,input().split()))
 
 count = 0
 
-# depth : 현재 몇 번째 원소까지 탐색했는지를 나타내는 인덱스
-# total : 지금까지 선택한 원소들의 합
-def dfs (depth, total) : 
+def dfs (index, total) :
     global count
-    if depth == n :
+    if index == n :
         if total == s :
             count += 1
-        return # 깊이가 n 이면 이미 다 돌아본 것이기 때문에 종료
+        return
     
-    # 현재 원소를 포함하는 경우
-    dfs(depth + 1, total + data[depth])
+    # 현재 원소를 포함할 때
+    dfs (index + 1, total + data[index])
     
-    # 현재 원소를 포함하지 않는 경우
-    dfs(depth + 1, total)
+    # 현재 원소를 포함하지 않을 때
+    dfs (index + 1, total)
 
-
-
-dfs(0,0)
-
-# 공집합도 total == 0으로 포함되므로, s == 0일 때는 1개 빼줌
-if s == 0:
+if s == 0 :
     count -= 1
-
+    
+dfs(0,0)
 print(count)
