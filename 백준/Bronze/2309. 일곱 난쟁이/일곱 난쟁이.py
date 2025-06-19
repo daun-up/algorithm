12e54ae6605368e2a960f706bb7234d5d98a08d6
Jodaun <105338882+daun-up@users.noreply.github.com>
2025-06-19 21:13:33 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,23 +1,24 @@
-list = []
-result = []
-find_result = False
-sum = 0
-
-
-for i in range(9) :
-    tmp = int(input())
-    list.append(tmp)
-    sum += tmp
+data = []
+for _ in range(9) :
+    data.append(int(input()))
+sum_data = sum(data)
+found = False
     
 for i in range(9) :
-    for j in range(i+1, 9) :
-        tmp_sum = sum - list[i] - list[j]
-        if tmp_sum == 100 :
-            result.extend([list[i], list[j]])
-            find_result = True
+    for j in range(i + 1, 9) :
+        if sum_data - (data[i] + data[j]) == 100 :
+            tmp1, tmp2 = data[i], data[j]
+            found = True
             break
-    if find_result : break
+        if found == True :
+            break
+            
+while len(data) > 7 :
+    data.remove(tmp1)
+    data.remove(tmp2)
+    
+data.sort()
 
-list.sort()
-for ele in list :
-    if ele not in result : print(ele)
+for k in data :
+    print(k)
+        
