06c520b51bb427525575d6f677673bc0945eb8ff
Jodaun <105338882+daun-up@users.noreply.github.com>
2025-07-09 13:39:03 +0900

    [Gold V] Title: 동전 2, Time: 288 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,21 +1,13 @@
+# 동전 1 처럼 경우의 수가 아니라 동전의 최소 개수 구하기
 n, k = map(int,input().split())
+coins = [int(input()) for _ in range(n)]
 
-sset = set() # 중복 제거
-
-for _ in range(n) :
-    sset.add(int(input()))
-    
-INF = k + 1
-dp = [INF]*(k+1)
+INF = int(1e9)
+dp = [INF] * (k+1)
 dp[0] = 0
 
-for coin in sset :
-    for j in range(1, k + 1) :
-        if (j - coin) >= 0 :
-            dp[j] = min(dp[j], dp[j-coin]+1)
-
-answer = dp[k] 
-if answer == INF :
-    answer = -1
-
-print(answer)
+for coin in coins :
+    for i in range(coin,k+1) :
+        dp[i] = min(dp[i-coin] + 1, dp[i])
+        
+print(dp[k] if dp[k] != INF else -1)
