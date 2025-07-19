057c1fb63c2dffc80227ba10b972c58961433561
Jodaun <105338882+daun-up@users.noreply.github.com>
2025-07-19 14:29:14 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,11 +1,6 @@
-def is_palindrome(s, start, end):
-    if start >= end:
-        return 1
-    if s[start] != s[end]:
-        return 0
-    # 양 끝 문자가 같으면 안쪽 문자열을 재귀적으로 확인
-    return is_palindrome(s, start + 1, end - 1)
+word = list(str(input()))
 
-s = input().strip()
-
-print(is_palindrome(s, 0, len(s) - 1))
+if list(reversed(word)) == word:
+    print(1)
+else:
+    print(0)
