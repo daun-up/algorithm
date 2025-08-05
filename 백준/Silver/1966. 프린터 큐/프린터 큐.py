2764f718bb8887c9153e19cc7418a920927f4975
Jodaun <105338882+daun-up@users.noreply.github.com>
2025-08-05 17:51:58 +0900

    [Silver III] Title: 프린터 큐, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,31 +1,24 @@
-import sys
-from collections import deque
-input = sys.stdin.readline
+t = int(input())
 
-t = int(input()) # 테스트 케이스의 수
-
-for _ in range(t):
+for _ in range(t) :
     n, m = map(int,input().split())
-    queue = deque(map(int, input().split())) # 큐 만들기
-    idx = deque(range(0,n))
-
-
-    cnt = 0
-
-    while queue:
-        if (queue[0] == max(queue)):
-            cnt += 1
-            queue.popleft()
-
-            if (idx.popleft() == m):
-                print(cnt)
-                break
+    
+    data = list(map(int,input().split()))
+    
+    result = 1 # 인쇄 순서 (최소 1번부터 시작)
+    
+    while data :
+        if data[0] < max(data) :
+            data.append(data.pop(0)) # 맨 앞 문서를 꺼내서 뒤로 붙임
         else :
-            queue.append(queue.popleft())
-            idx.append(idx.popleft())
-
-
-
+            if m == 0 : break # 내가 찾던 문서가 인쇄됨 -> 바로 종료
+            
+            data.pop(0) # 아니라면 
+            result += 1
         
-
-
+        # 위치 업데이트 (문서의 위치가 계속 바뀌므로 찾고 있던 문서의 위치 이동)
+        if m > 0 : # 아직 뒤에 있으므로 그냥 한 칸 앞으로 이동
+            m = m - 1
+        else : # 맨 앞에 있었기 때문에 맨 뒤로 이동했으므로 길이 줄이기
+            m = len(data) - 1
+    print(result)
