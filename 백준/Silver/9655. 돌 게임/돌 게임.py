cb3987384f1b18576c30b8f8eb9a8beb47d16e61
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-09-02 20:42:48 +0900

    [Silver V] Title: 돌 게임, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.py
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -1,6 +1,25 @@
 n = int(input())
 
-if n % 2 == 0 :
-    print("CY")
+# 턴을 번갈아가면서 돌 가져감
+# 1 개 또는 3 개 가져갈 수 있음 <- 이거 식을 어떻게 세우지?
+# 마지막 돌 가져간 사람 WIN
+# 상근이가 먼저 시작
+
+# dp = [0 for i in range(n+1)]
+
+# dp[1] = 1
+# dp[2] = 2
+# dp[3] = 1
+
+# for i in range(4, n+1) :
+#     dp[n] = min(dp[n-1], dp[n-3]) + 1
+    
+# if dp[n] % 2 == 1 :
+#     print('SK')
+# else :
+#     print('CY')
+
+if n % 2 == 1 :
+    print('SK')
 else :
-    print("SK")
+    print('CY')
