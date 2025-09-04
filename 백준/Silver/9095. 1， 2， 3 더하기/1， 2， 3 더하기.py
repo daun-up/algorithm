0430da27206bea960c1734797dacf66dc18eac3f
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-09-04 20:42:52 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,5 +1,17 @@
-dp = [0,1,2,4]
-for _ in range(4,14):   
-    dp.append(sum(dp[-3:]))
-for _ in range(int(input())):    
-    print(dp[int(input())])
+t = int(input())
+
+dp = [0] * 11
+        
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+ 
+for i in range(4, 11) :
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]    
+    
+for i in range(t) :
+    n = int(input())
+    
+    # n 을 1,2,3 의 합으로 나타내는 방법의 수 출력
+    print(dp[n])
+
