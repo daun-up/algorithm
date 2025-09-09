a35ae3889c644dc0d2115242a131462177a8dc44
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-09-09 23:09:05 +0900

    [Silver I] Title: RGB거리, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,16 +1,18 @@
 n = int(input())
+costs = [list(map(int,input().split())) for _ in range(n)]
 
-# for i in range(n) :
-#     data = list(map(int,input().split()))
+dp = [[0] * 3 for _ in range(n)]
 
-data = [list(map(int,input().split())) for _ in range(n)]
+dp[0] = costs[0] # 첫 행은 비용 그대로
 
-dp = [[0] * 3 for _ in range(n)] # 0 으로 dp 초기화
-dp[0] = data[0]
+for i in range(1,n) :
+    # r,g,b = map(int,input().split())
+    
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + costs[i][0]
+    # i 번째 집을 빨강 (0) 으로 칠한다고하면, 이전 집은 초록(1)이나 파랑(2)만 가능
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + costs[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + costs[i][2]
 
-for i in range(1, n) :
-    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + data[i][0] # 빨간색 0
-    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + data[i][1] # 초록색 1
-    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + data[i][2] # 파란색 2
+result = min(dp[n-1]) # 모든 집 (n개) 를 다 칠했을 때 최소 비용은 마지막 집을 어떤 색으로 칠했는지에 따라 달라짐
 
-print(min(dp[-1]))
+print(result)
