42389db57706be0a18423b51a16811b9497dfbd5
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-09-10 23:51:25 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 35068 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,24 +1,35 @@
-def bfs(si, sj, ei, ej):
-    q = []
-    v = [[0]*M for _ in range(N)]
+from collections import deque
 
-    q.append((si,sj))
-    v[si][sj] = 1
+n, m = map(int,input().split())
+graph = [list(map(int,input())) for _ in range(n)]
 
-    while q:
-        ci, cj = q.pop(0)
-        if (ci,cj) == (ei,ej):  # 정답 처리가 필요한 경우 이자리에서..
-            return v[ei][ej]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-        # 4방향, 범위내, 조건에 맞으면: arr == 1, v == 0
-        for di,dj in ((-1,0), (1,0), (0,-1), (0,1)):
-            ni, nj = ci+di, cj+dj
-            if 0<=ni<N and 0<=nj<M and arr[ni][nj]==1 and v[ni][nj]==0:
-                q.append((ni,nj))
-                v[ni][nj] = v[ci][cj]+1
+def bfs(x,y) :
+    queue = deque()
+    queue.append((x, y))
+    
+    while queue :
+        x, y = queue.popleft()
+        
+        # 상하좌우 확인
+        for i in range(4) :
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            # graph 를 삐져나갈 경우
+            if nx < 0 or ny < 0 or nx >= n or ny >= m :
+                continue
+            
+            # 0 일 때는 못 가
+            if graph[nx][ny] == 0 :
+                continue
+            
+            if graph[nx][ny] == 1:  # 아직 방문하지 않은 길
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+                
+    return graph[n-1][m-1] # 
 
-N, M = map(int, input().split())
-arr = [list(map(int, input())) for _ in range(N)]
-
-ans = bfs(0, 0, N-1, M-1)
-print(ans)
+print(bfs(0,0)) # 0,0 에서 시작하니까
