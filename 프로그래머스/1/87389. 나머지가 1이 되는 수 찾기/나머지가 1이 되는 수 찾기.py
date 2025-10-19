b6a9ab4c8522c6919d4b79326b89a2707644d3af
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-19 21:40:25 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 56.02 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
@@ -1,10 +1,14 @@
 def solution(n):
-    answer = 0
-    i = 0
-    for i in range(1,n):
-        if n % i == 1:
-            answer = i
-            break
+    # 리스트로 만들어서 첫 번째 요소를 꺼냄
+    return [x for x in range(1, n) if n % x == 1][0]
+
+#     answer = 0
+#     i = 0
+#     for i in range(1,n):
+#         if n % i == 1:
+#             answer = i
+#             break
         
         
-    return answer
+#     return answer
+
