1d7fc5dfb4b04a45a73f31940f1b38a89666350f
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-19 21:44:36 +0900

    [level 1] Title: 약수의 합, Time: 0.17 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,8 +1,12 @@
 def solution(n):
-    answer = 0
-    tmp = []
-    for i in range(1,n+1):
-        if n % i == 0:
-            tmp.append(i)
     
-    return sum(tmp)
+    # num / 2 의 수들만 검사하면 성능 약 2배 향상
+    return n + sum([i for i in range(1,(n//2) + 1) if n % i == 0])
+
+#     answer = 0
+#     tmp = []
+#     for i in range(1,n+1):
+#         if n % i == 0:
+#             tmp.append(i)
+    
+#     return sum(tmp)
