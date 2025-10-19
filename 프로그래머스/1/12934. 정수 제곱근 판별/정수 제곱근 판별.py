d15f1eadb0d54479fa3b9e4b63daa5817be920a1
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-19 23:57:08 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,11 +1,8 @@
 def solution(n):
-    answer = 0
-    tmp = int(n ** 0.5)
-    if n == tmp ** 2 :
-        answer = (tmp+1)**2
-    else :
-        answer = -1
-    return answer
-
-# x값이 실수가 되는 케이스가 있기에 n의 제곱근을 정수화 시키는 것이 핵심
-# 정수화 하지 않으면 x 값이 실수가 되는 케이스가 있고, return 값도 실수가 나오므로 오답이 됨
+    # 제곱 판별 어떻게 하더라
+    tmp = int(n)**(1/2)
+    
+    if tmp == int(tmp): # 정수 x 일 때
+        return (tmp+1)**2
+    else:
+        return -1
