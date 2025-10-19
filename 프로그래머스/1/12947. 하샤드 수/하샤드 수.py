410d7b8f590155967fe3a52b11a82392f32dc478
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-19 23:54:27 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -1,10 +1,12 @@
 def solution(x):
-    answer = True
-    ls = [int(i) for i in str(x)]
-    s = sum(ls)
+    return x % sum(int(x) for x in str(x)) == 0
+
+#     answer = True
+#     ls = [int(i) for i in str(x)]
+#     s = sum(ls)
     
-    if (x % s) == 0:
-        answer = True
-    else:
-        answer = False
-    return answer
+#     if (x % s) == 0:
+#         answer = True
+#     else:
+#         answer = False
+#     return answer
