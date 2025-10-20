b9231f7f6db95ab612762134756d99afef848a11
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-20 11:34:05 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,6 +1,5 @@
 def solution(seoul):
     answer = ''
-    for i in range(len(seoul)) :
-        if seoul[i] == 'Kim' :
-            answer = "김서방은 "+str(i)+"에 있다"
-    return answer
+    for i in range(len(seoul)):
+        if seoul[i] == "Kim":
+            return f"김서방은 {i}에 있다"
