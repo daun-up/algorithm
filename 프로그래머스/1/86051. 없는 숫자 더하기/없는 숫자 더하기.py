9e924ee04c87c2e34a059cbea0d89f5df0de6ae5
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-20 11:25:34 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,9 +1,10 @@
 def solution(numbers):
-    answer = 0
-    # numbers = numbers.sort()
+    return 45 - sum(numbers)
+#     answer = 0
+#     # numbers = numbers.sort()
     
-    for n in range(10):
-        if n not in numbers:
-            answer += n
+#     for n in range(10):
+#         if n not in numbers:
+#             answer += n
     
-    return answer
+#     return answer
