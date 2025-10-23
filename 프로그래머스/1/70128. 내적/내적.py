e806d2d8a59c2b512310f68696a58045cfd8166b
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-23 12:18:39 +0900

    [level 1] Title: 내적, Time: 0.14 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.py
+++ 프로그래머스/1/70128. 내적/내적.py
@@ -1,7 +1,12 @@
 def solution(a, b):
+    print(zip(a,b))
+    
+    # zip(): 동일한 인덱스끼리 묶어 튜플로 만들어 반환
+    return sum([x*y for x,y in zip(a,b)])
+
     # answer = [0]*len(a)
-    answer = []
-    for i in range(len(a)):
-        answer.append(a[i] * b[i])
-        # 만약 
-    return sum(answer)
+    # answer = []
+    # for i in range(len(a)):
+    #     answer.append(a[i] * b[i])
+    #     # 만약 
+    # return sum(answer)
