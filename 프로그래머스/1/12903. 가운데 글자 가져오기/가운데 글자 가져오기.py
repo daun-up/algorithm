2a339b4c8a4732cebd9863af1a9ba6634a9a55e5
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-23 10:59:17 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,13 +1,11 @@
 def solution(s):
     answer = ''
-    tmp = s.split()
-    center = int(len(s) / 2)
-    for i in range(len(s)) :
-        if len(s) % 2 == 0 :
-            if i == center :
-                answer += s[i-1]
-                answer += s[i]
-        else :
-            if i == center :
-                answer += s[i]
+    s = list(s)
+    tmp = len(s) // 2
+
+    if len(s) % 2 == 0:
+        answer += s[tmp-1]
+        answer += s[tmp]
+    else:
+        answer += s[tmp]
     return answer
