e2e70c90d5edcde9a186dff75db8c8273a1382ce
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-23 12:27:36 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 27.06 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,13 +1,14 @@
 def solution(left, right):
     answer = 0
-    for i in range(left , right + 1) :
-        tmp = 0
-        for j in range(1, i+1) :
-            if i % j == 0 :
-                tmp += 1
-        if tmp % 2 == 0 :
+    for i in range(left, right+1):
+        cnt = 0
+        for j in range(1,i+1):
+            if i % j == 0:
+                cnt += 1
+        if cnt % 2 == 0:
             answer += i
-        else :
+        else:
             answer -= i
-        
     return answer
+
+# 약수 구하는 방법
