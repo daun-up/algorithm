1564d0a026f27e17830bfc9b8bfcf2b008ff56d9
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-25 10:52:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.99 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,9 +1,21 @@
 def solution(participant, completion):
-    answer = ''
-    participant.sort()
-    completion.sort()
+    tmp = 0
+    dic = {}
     
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
-    return participant[-1]
+    for p in participant:
+        dic[hash(p)] = p # 이름 값을 hash 로 저장
+        tmp += hash(p) # 참가자들의 hash 값을 더함
+        
+    for c in completion:
+        tmp -= hash(c)
+    
+    return dic[tmp] # 남은 해시 값 = 완주하지 못한 사람의 해시
+
+#     answer = ''
+#     participant.sort()
+#     completion.sort()
+    
+#     for i in range(len(completion)):
+#         if participant[i] != completion[i]:
+#             return participant[i]
+#     return participant[-1]
