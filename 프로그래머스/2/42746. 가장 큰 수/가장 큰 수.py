00a78fc670d88176d3296c9933ff402f76c46a1b
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-25 22:29:31 +0900

    [level 2] Title: 가장 큰 수, Time: 60.95 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,20 @@
+# def cmp(a, b):
+#     if a + b > b + a:
+#         return True   # a가 앞에 와야 함
+#     else:
+#         return False  # b가 앞에 와야 함
+
+def solution(numbers):
+    nums = list(map(str, numbers))
+    
+    nums.sort(key = lambda x : x*10, reverse=True)
+    answer = ''.join(nums)
+
+#     for i in range(len(nums)):
+#         for j in range(i + 1, len(nums)):
+#             if not cmp(nums[i], nums[j]):
+#                 nums[i], nums[j] = nums[j], nums[i]
+
+#     answer = ''.join(nums)
+    
+    return '0' if answer[0] == '0' else answer
