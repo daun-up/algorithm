e0edc98d48a0530822df64ae4277c991e820d0e0
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-26 21:47:29 +0900

    [level 2] Title: 전화번호 목록, Time: 455.27 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,13 +1,28 @@
 def solution(phone_book):
     answer = True
+    hash = {}
+    
+    for phone_number in phone_book:
+        hash[phone_number] = 1
+    
+    for phone_number in phone_book:
+        tmp = ""
+        for n in phone_number:
+            tmp += n
+            
+            if tmp in hash and tmp != phone_number:
+                answer = False
+            
+    
     # for i in range(0,len(phone_book)):
     #     for j in range(i+1, len(phone_book)):
     #         if phone_book[j].startswith(phone_book[i]):
     #             answer = False
     
-    phone_book.sort()
-    for i in range(len(phone_book)-1):
-        if phone_book[i+1].startswith(phone_book[i]):
-            answer = False
+#     phone_book.sort()
+#     for i in range(len(phone_book)-1):
+#         if phone_book[i+1].startswith(phone_book[i]):
+#             answer = False
         
     return answer
+
