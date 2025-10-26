16ab3c895b0bc03bfa23c6a487f086f5185920c0
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-26 22:56:14 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,3 +1,17 @@
 def solution(brown, yellow):
     answer = []
+    w, h = 0,0
+    for i in range(1, yellow + 1):
+        if yellow % i == 0:
+            w = yellow // i
+            h = i
+            if (2*w + 2*h + 4) == brown:
+                answer.append(w+2)
+                answer.append(h+2)
+                break
+                
+                
+    answer.sort(reverse=True)
+    
+    
     return answer
