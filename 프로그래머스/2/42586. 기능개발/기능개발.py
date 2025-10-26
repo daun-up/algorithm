9b6fd2d554913aa8850dede9d8834e070f0b1117
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-26 14:28:24 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def solution(progresses, speeds):
+    answer = []
+    cnt = 0
+    time = 0
+    
+    while len(progresses) > 0:
+        if progresses[0] + time*speeds[0] >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt += 1
+        else:
+            if cnt > 0:
+                answer.append(cnt)
+                cnt = 0
+            time += 1
+    answer.append(cnt)
+    
+    return answer
