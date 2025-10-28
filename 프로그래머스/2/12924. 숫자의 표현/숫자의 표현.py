5f384ac61bb3bcb7a14e731d014e769f93528b7b
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-28 09:44:43 +0900

    [level 2] Title: 숫자의 표현, Time: 6.13 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,12 +1,13 @@
 def solution(n):
     answer = 0
-    for i in range(1,n+1) :
+    for i in range(1,n+1): # 시작 숫자
         sum = 0
-        for j in range(i,n+1) :
+        for j in range(i,n+1): # 연속 합
             sum += j
-            if sum == n :
+            
+            if sum == n: # 정확히 n 이 되면
                 answer += 1
-                break
-            elif sum > n :
-                break
+                break # 안쪽 for 문
+            elif sum > n: # n 을 초과하면 멈춤
+                break # 안쪽 for 문
     return answer
