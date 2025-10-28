e91e96e0ad360db7e7aa673047ae85933173a2cf
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-28 09:33:47 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 2.00 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,15 +1,19 @@
 def solution(s):
     answer = []
     cnt = 0
-    zero = 0
-
-    while s != '1' :
-        zero += s.count("0")
-        s = s.replace('0','') # 1. 0 제거
+    zero_cnt = 0
+    
+    # s가 "1"이 될 때까지 계속해서
+    while s != '1':
+        zero_cnt += s.count('0') # 0 의 개수 세기
         
-        s = bin(len(s))[2:]      #2진수로 변환
+        s = s.replace('0','') # replace
+        
+        s = bin(len(s))[2:] # 이진수 만들기
+        
         cnt += 1
-        
+    
     answer.append(cnt)
-    answer.append(zero)
+    answer.append(zero_cnt)
+    
     return answer
