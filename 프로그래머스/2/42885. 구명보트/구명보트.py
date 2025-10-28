6aa90276ee57908d6155cf5dba1ba0c5d6bced4d
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-28 21:55:55 +0900

    [level 2] Title: 구명보트, Time: 9.27 ms, Memory: 9.71 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,11 +1,16 @@
 def solution(people, limit):
     answer = 0
+    # 한 번에 두 명
     people.sort()
-    a = 0
-    b = len(people) - 1
-    while a < b :
-        if people[a] + people[b] <= limit :
-            a += 1
-            answer += 1
-        b -= 1
-    return len(people) - answer
+    l, r = 0, len(people) - 1
+    
+    # while l < r: <- 두 명 이상이 남았을 때만 처리 (마지막 1명은 처리 안 됨)
+    while l <= r: # <- 한 명 남았을 때까지 처리 (마지막 1명도 보트 태움!)
+        if people[l] + people[r] <= limit:
+            l += 1
+            r -= 1
+        else:
+            r -= 1
+        answer += 1
+    
+    return answer
