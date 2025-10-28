b1688363605f1e40609e2652106ee4c3f644b1d0
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-28 09:03:21 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.26 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,7 +1,9 @@
 def solution(A,B):
     answer = 0
-    A.sort()
     B.sort(reverse=True)
-    for i in range(len(A)) :
-        answer += A[i] * B[i]
+    A.sort()
+
+    for a, b in zip(A,B):
+        answer += a*b
+
     return answer
