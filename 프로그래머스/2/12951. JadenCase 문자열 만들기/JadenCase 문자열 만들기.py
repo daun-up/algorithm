b78843c01910e55faa686f89bea594894043bcb2
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-10-28 09:16:06 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,13 +1,17 @@
 def solution(s):
     answer = ''
-    s = list(s)
+    # s = list(s) # str 타입은 불변 객체라서 문자열의 한 글자를 직접 바꾸는 건 불가능
     
     for i in range(len(s)):
         if s[i-1] == ' ' or i == 0:
-            s[i] = s[i].upper()
+            # s[i] = s[i].upper()
+            answer += s[i].upper()
         else:
-            s[i] = s[i].lower()
+            # s[i] = s[i].lower()
+            answer += s[i].lower()
+    
+    
             
-    answer = ''.join(s)
+    # answer = ''.join(s)
     
     return answer
