888ec0cd87fd6d663517d7a53dc5a8c37bf79b0c
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-11-03 15:21:56 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.80 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=python3) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.47 ms
+메모리: 9.09 MB, 시간: 0.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 03일 14:32:17
+2025년 11월 03일 15:21:54
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,7 +1,9 @@
-# 서로 다른 두 개의 수를 뽑아 더해서 만들 수 있는 경우 (조합)
 def solution(numbers):
-    answer = set()
+    answer = []
     for i in range(len(numbers)):
-        for j in range(i + 1, len(numbers)):
-            answer.add(numbers[i] + numbers[j])
-    return sorted(answer)
+        for j in range(i+1,len(numbers)):
+            answer.append(numbers[i] + numbers[j])
+    answer = sorted(set(answer))
+    
+        
+    return answer
