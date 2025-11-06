48c9bbd328851083b62462b9c255458a7cad64ca
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-11-06 20:16:29 +0900

    [level 1] Title: 삼총사, Time: 0.10 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,10 +1,7 @@
-# 3 개 수를 더해서 0 이 되는 조합을 찾는 것
+from itertools import combinations
 def solution(number):
     answer = 0
-    n = len(number)
-    for i in range(n - 2):
-        for j in range(i + 1, n - 1):
-            for k in range(j + 1, n):
-                if number[i] + number[j] + number[k] == 0:
-                    answer += 1
+    for comb in combinations(number, 3):
+        if sum(comb) == 0:
+            answer += 1
     return answer
