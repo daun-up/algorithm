bb204bc134708d6964ca5eda78a0136e2e1be211
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-11-06 20:44:11 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.79 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,12 +1,13 @@
-# dictionary
 def solution(s):
     answer = []
-    dict = {}
-    for i in range(len(s)) :
-        char = s[i]
-        if char in dict :
-            answer.append(i - dict[char])
-        else :
+    dic = {} # 각 문자별 마지막 등장 위치
+    
+    for i, ch in enumerate(s):
+        if ch in dic:
+            answer.append(i - dic[ch]) # 현재 인덱스 빼기 마지막 등장 인덱스
+        else:
             answer.append(-1)
-        dict[char] = i
+        dic[ch] = i # 마지막 위치 갱신
+            
+            
     return answer
