def9cc21d51fa6dcb585187f24ef0116f2b8bb44
Daun Jo <105338882+daun-up@users.noreply.github.com>
2025-11-06 20:26:49 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.06 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -11,9 +11,24 @@
 #         answer += s
 #     return answer
 
+# def solution(s):
+#     answer = []
+#     for word in s.split(' '):  # 공백 포함 split
+#         new_word = ''
+#         for i, ch in enumerate(word):
+#             if i % 2 == 0:
+#                 new_word += ch.upper()
+#             else:
+#                 new_word += ch.lower()
+#         answer.append(new_word)
+#     return ' '.join(answer)
+
+# split(' ') → join(' ') 구조를 쓰면 원래의 공백 구조가 1:1로 복원
+
 def solution(s):
     answer = []
-    for word in s.split(' '):  # 공백 포함 split
+    
+    for word in s.split(' '):
         new_word = ''
         for i, ch in enumerate(word):
             if i % 2 == 0:
@@ -22,5 +37,3 @@ def solution(s):
                 new_word += ch.lower()
         answer.append(new_word)
     return ' '.join(answer)
-
-# split(' ') → join(' ') 구조를 쓰면 원래의 공백 구조가 1:1로 복원
