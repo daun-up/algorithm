5340290b6faee8572f938e81974f19b3e044f2dc
Daun Jo <awhekdns@kookmin.ac.kr>
2026-03-24 23:18:25 +0900

    [Silver I] Title: 안전 영역, Time: 280 ms, Memory: 55372 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,6 +1,6 @@
 
 /**
- * 2468. 안전 영역
+ * 2468. 안전 영역 (refactoring)
  */
 import java.util.*;
 import java.io.*;
@@ -9,50 +9,39 @@ public class Main {
     static int N;
     static int[][] map;
     static boolean[][] visited;
+
     static int maxHeight;
-    static int cnt;
-    static Queue<int[]> queue;
-    static int maxCnt;
+    static int maxCnt; // 최대 안전 영역
 
     static int[] dr = { -1, 1, 0, 0 };
     static int[] dc = { 0, 0, -1, 1 };
 
-    // maxHeight 이하는 잠기게 하는 함수
-    static void water(int height) {
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < N; c++) {
-                if (map[r][c] <= height) {
-                    map[r][c] = -1; // 물에 잠김
-                }
-            }
-        }
-
-    }
-
     // 잠길 거 다 잠겼을 때 안전 영역을 세는 함수
-    static void bfs() {
-        // queue 에 더 이상 없을 때
-        cnt++;
+    static void bfs(int r, int c, int h) {
+        // 시작점 넣기
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] { r, c });
+        visited[r][c] = true;
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
 
-            int cr = cur[0];
-            int cc = cur[1];
-
             for (int d = 0; d < 4; d++) {
-                int nr = cr + dr[d];
-                int nc = cc + dc[d];
+                int nr = cur[0] + dr[d];
+                int nc = cur[1] + dc[d];
 
-                if (nr >= 0 && nr < N && nc >= 0 && nc < N) {
-                    if (!visited[nr][nc] && map[nr][nc] != -1) {
-                        queue.add(new int[] { nr, nc });
+                if (inRange(nr, nc)) {
+                    if (!visited[nr][nc] && map[nr][nc] > h) {
                         visited[nr][nc] = true;
+                        queue.add(new int[] { nr, nc });
                     }
                 }
             }
         }
+    }
 
+    static boolean inRange(int r, int c) {
+        return r >= 0 && r < N && c >= 0 && c < N;
     }
 
     public static void main(String[] args) throws Exception {
@@ -63,11 +52,7 @@ public class Main {
         // 지역 초기화
         map = new int[N][N];
 
-        maxHeight = 0;
-        maxCnt = 1; // 아무 곳도 물에 잠기지 않았을 경우
-
-        int[][] originalMap = new int[N][N];
-
+        int maxHeight = 0;
         // 지역의 높이 입력 받음
         for (int r = 0; r < N; r++) {
             st = new StringTokenizer(br.readLine());
@@ -75,32 +60,27 @@ public class Main {
                 map[r][c] = Integer.parseInt(st.nextToken());
                 maxHeight = Math.max(map[r][c], maxHeight);
             }
-            originalMap[r] = map[r].clone();
         }
 
-        queue = new LinkedList<>();
+        maxCnt = 1;
 
-        for (int i = 1; i <= maxHeight; i++) {
-            cnt = 0;
+        // 높이 1 부터 (maxHeight - 1) 까지 탐색
+        for (int h = 1; h < maxHeight; h++) {
             visited = new boolean[N][N];
-            water(i);
+            int cnt = 0;
             // 전체 경우의 수 bfs 돌기
             for (int r = 0; r < N; r++) {
                 for (int c = 0; c < N; c++) {
-                    if (map[r][c] != -1 && !visited[r][c]) {
+                    // 높이 h보다 크고, 아직 방문하지 않은 경우에만 bfs 시작
+                    if (map[r][c] > h && !visited[r][c]) {
                         // 시작점 큐에 넣기
-                        queue.add(new int[] { r, c });
-                        bfs();
+                        bfs(r, c, h);
+                        cnt++;
                     }
                 }
             }
 
             maxCnt = Math.max(cnt, maxCnt);
-
-            // map 초기화
-            for (int r = 0; r < N; r++) {
-                map[r] = originalMap[r].clone();
-            }
         }
 
         System.out.print(maxCnt);
