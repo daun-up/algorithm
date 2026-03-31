2a3e5e2a87a6e4e3bc2b88d9b995aa19c811c78a
Daun Jo <awhekdns@kookmin.ac.kr>
2026-04-01 00:27:21 +0900

    [Silver II] Title: N과 M (10), Time: 100 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,59 @@
+
+/**
+ * N과 M (10)
+ * 중복 조합
+ */
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] output;
+    static StringBuilder sb;
+
+    static void choose(int idx, int cnt) {
+        if (cnt == M) {
+            for (int val : output) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int flag = 0;
+
+        for (int i = idx; i <= N; i++) {
+            if (arr[i] != flag) {
+                output[cnt] = arr[i];
+                flag = arr[i];
+                choose(i + 1, cnt + 1);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        // 입력
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        // 전역 변수 초기화
+        arr = new int[N + 1];
+        output = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        choose(1, 0);
+
+        System.out.println(sb);
+    }
+}
