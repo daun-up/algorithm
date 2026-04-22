851ebc823d0f97ee6af64da7df22e66deacc6f05
Daun Jo <awhekdns@kookmin.ac.kr>
2026-04-23 00:04:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 50.26 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,21 +1,20 @@
+from collections import defaultdict
 def solution(participant, completion):
-    tmp = 0
-    dic = {}
+    answer = ''
+    people = defaultdict(int)
     
-    for p in participant:
-        dic[hash(p)] = p # 이름 값을 hash 로 저장
-        tmp += hash(p) # 참가자들의 hash 값을 더함
-        
-    for c in completion:
-        tmp -= hash(c)
+    for name in participant:
+        people[name] += 1
     
-    return dic[tmp] # 남은 해시 값 = 완주하지 못한 사람의 해시
-
-#     answer = ''
-#     participant.sort()
-#     completion.sort()
+    for name in completion:
+        people[name] -= 1
     
-#     for i in range(len(completion)):
-#         if participant[i] != completion[i]:
-#             return participant[i]
-#     return participant[-1]
+    # print(people)
+    # print(list(people.items()))
+    # print(dict(people))
+    
+    for k, v in people.items():
+        if v >= 1:
+            answer = k
+    
+    return answer
