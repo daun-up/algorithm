678a3b22c921add83c4aa95d91ccb8fd8c263e64
Daun Jo <awhekdns@kookmin.ac.kr>
2026-04-22 23:07:38 +0900

    [level 2] Title: 소수 찾기, Time: 8.23 ms, Memory: 9.46 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,25 +1,33 @@
 from itertools import permutations
 
 def isPrime(x):
-    x = int(x)
+    # 1과 자기 자신으로만 나누어떨어지는 수
     if x < 2:
         return False
-    for i in range(2, int(x**0.5) + 1):
+        
+    for i in range(2, int(x ** 0.5) + 1):
+        # 모두 확인하면 비효율적이니까 제곱근까지만 확인해도 됨
         if x % i == 0:
             return False
+        
     return True
-
+    
 def solution(numbers):
     answer = 0
-    tmp = set()
+    nums = set()
     
-    for i in range(1,len(numbers)+1):
-        for num in permutations(numbers,i):
-            # tmp.add(''.join(num))
-            tmp.add(int(''.join(num))) 
-
-    for t in tmp:
-        if isPrime(t):
+    # for i in range(1, len(numbers) + 1):
+    #     nums.add(int(''.join(permutations(numbers, i))))
+    
+    for i in range(1, len(numbers) + 1):
+        # print(list(permutations(numbers, i)))
+        for p in permutations(numbers, i):
+            # print(p)
+            nums.add(int(''.join(p)))
+    
+    for num in nums:
+        # print(num)
+        if isPrime(num):
             answer += 1
-    
+            
     return answer
