136456ed5c6612c6173a2f5df7d26f216bc3d0d4
Daun Jo <awhekdns@kookmin.ac.kr>
2026-04-23 23:01:13 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,13 +1,17 @@
 def solution(n):
     ans = 0
-    
-    while True :
-        if n == 0 :
-            break
-        if n % 2 == 0 :
-            n //= 2
-        else :
+    # Top-down
+    while n > 0:
+        if n % 2 == 0:
+            n = n // 2
+        # 내가 지금 4에 있다면 2에서 순간이동해서 왔구나 알 수 있으니까
+        else:
             n -= 1
-            ans += 1
+            ans += 1 # 비용이 들음
 
     return ans
+
+# [점프] 한 번에 K 칸 
+# [순간이동] (현재까지 온 거리) * 2 
+
+# 그리디?
