084ce485407b7e050b28803941771596329e55c6
Daun Jo <awhekdns@kookmin.ac.kr>
2026-05-08 18:22:38 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,16 +1,8 @@
 def solution(array, commands):
     answer = []
-    # commands = [i,j,k]
     for command in commands:
-        # print(command)
-        i,j,k = command
-        # sliced = array[i-1:j]
-        # sliced.sort()
-        # answer.append(sliced[k-1])
-        answer.append(list(sorted(array[i-1:j]))[k-1])
-            
+        i, j, k = command
+        tmp = array[i - 1:j]
+        tmp.sort()
+        answer.append(tmp[k - 1])
     return answer
-
-# 파이썬에서 슬라이싱을 할 때는 첫 번째 인덱스는 포함하고 끝 인덱스는 포함하지 않음
-# k번째 수(1-based) -> 0-based -> k-1
-# i번째 수(1-based) -> 0-based -> i-1
